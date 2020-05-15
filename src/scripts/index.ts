15abda72374961384557046eb3e04e6c002c943c
Robert Easley <roberteasley@Tessas-iMac.local>
2020-05-15 15:01:53 -0700

    Removed uneaded folder

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -2,5 +2,4 @@ import {list} from './modules/list'
 test(list)
 function test (listTest:string[]) {
     console.log(`Hello ${listTest[1]}`)
-
 }
--- static-base/src/index.11tydata.js
+++
@@ -1,7 +0,0 @@
-module.exports = function() {
-  return {
-		"eleventyExcludeFromCollections": true,
-		"layout": "layouts/base",
-		"title": "Title Strings"	
-	};
-};
